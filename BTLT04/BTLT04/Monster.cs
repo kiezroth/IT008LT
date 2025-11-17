af2a8454f61150e926029d73ecc32eabe35e97f3
kiezroth <izayoizero01@gmail.com>
2025-11-17 19:32:34 +0700

    Add files via upload

---
+++ BTLT04/BTLT04/Monster.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Security.Policy;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BTLT04
+{
+    class Monster : Entity
+    {
+        public AnimatedSprite Sprite;
+        public Monster(float x, float y) : base(x, y, 64, 64)
+        {
+            Width = 64; 
+            Height = 64; // hitbox Monster
+            //Sprite = new AnimatedSprite(fixPath(@"<Đường dẫn>"), <FrameWidth>, <FrameHeight>, <Số frame 1 hàng>);
+        }
+        public override void Update() 
+        {
+            Sprite.NextFrame();
+            //Hành động
+        }
+
+        public override void Render(Graphics g)
+        {
+            Sprite.Draw(g, X, Y, Width, Height);
+        }
+    }
+}
